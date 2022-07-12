90f1fd3e921ccee63ced2a3ec106548f70f4594e
cAmartya <80196675+cAmartya@users.noreply.github.com>
2022-07-12 18:12:25 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
+const url = process.env.SERVER_PORT;
 
 const API = axios.create({baseURL: url}) 
 API.interceptors.request.use((req) =>    {
     if(localStorage.getItem('profile')) {
         req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
-        // req.header.Authorization = `Bearer ${localStorage.getItem('profile')}`
     }
 
     return req;
