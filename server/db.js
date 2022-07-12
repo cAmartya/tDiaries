6afe7dcbee1335e2e8c6f91fe2310d6f4afb124c
cAmartya <80196675+cAmartya@users.noreply.github.com>
2022-07-12 18:11:01 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-// const MONGO_URL = 'mongodb://localhost:27017/Diaries?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL;
@@ -11,6 +10,3 @@ const connectToMongo = () => {
 };
 
 export default connectToMongo;
-
-
-// const mongoURI = 'mongodb+srv://amartya:Amartya.6411@cluster0.kvdsk.mongodb.net/?retryWrites=true&w=majority';
