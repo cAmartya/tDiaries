6d44242587512aedad2ea885a2f6dc6df1b0f78b
cAmartya <80196675+cAmartya@users.noreply.github.com>
2022-12-03 00:16:21 +0530

    Create postMessage.js

---
+++ server/models/postMessage.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    message: String,
+    userName: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
+    createdAt: { type: Date, default: new Date() },
+})
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
